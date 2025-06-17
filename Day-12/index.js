cdc0e858afe23654af1c86864f3d0a31766a4ab7
Dip290703 <dipanshu6233@gmail.com>
2025-06-17 13:07:37 +0530

    js question

--- Day-12/index.js
+++ Day-12/index.js
@@ -1,1 +1,22 @@
-console.log('js question');
+// prime number
+function isPrime(inputNumber) {
+  let result = true;
+  if(inputNumber === 2){
+    result = true;
+  }
+  else if (inputNumber % 2 === 0) {
+    result = false;
+  }
+
+  if (result === true) {
+    for (let i = 3; i < inputNumber; i += 2) {
+      if (inputNumber % i === 0) {
+        result = false;
+        break;
+      }
+    }
+  }
+  return result;
+}
+
+console.log(isPrime(2));
