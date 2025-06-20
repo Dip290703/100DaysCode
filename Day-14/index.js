ba5cff61959fa2f88c02a3a5f74520d0e146e7ac
Dip290703 <dipanshu6233@gmail.com>
2025-06-20 10:15:50 +0530

    js

---
+++ Day-14/index.js
@@ -0,0 +1,22 @@
+function celsiusToFaherniet(input){
+let fahrenheit = (input * 9/5) + 32;
+return fahrenheit;
+}
+
+console.log(celsiusToFaherniet(45));
+function fahrenheitToCelsius(input){
+    let celsius = (input - 32) * 5/9;
+    return celsius;
+}
+console.log(fahrenheitToCelsius(113));
+
+//swappinig with extra variable
+
+let a = 5 
+let b = 4
+let c 
+c=a 
+a= b
+b=c
+console.log(a);
+console.log(b);
